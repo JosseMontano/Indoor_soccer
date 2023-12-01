b3e6d8b34fd5d4912737c3548ac3d3ad6733ea10
eljosema505 <eljosema505@gmail.com>
2023-12-01 17:54:14 -0400

    bk:add model Team

---
+++ backend/backend/Controllers/TeamController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using backend.Context;
+using backend.Models
+
+namespace backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class TeamController
+    {
+        private DBCont _dbCont;
+        public TeamController(DBCont dbCont)
+        {
+            _dbCont = dbCont;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+          /*  var teams = _dbCont.;
+            return Ok(cars);*/
+        }
+
+
+
+
+
+    }
+}
--- backend/backend/Models/User.cs
+++ backend/backend/Models/User.cs
@@ -1,12 +1,10 @@
 ﻿using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 namespace backend.Models
 {
     public class User
     {
         [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public Guid Id { get; set; }
+        public int Id { get; set; }
         [Required]
         [StringLength(25)]
         public string UserName { get; set; }
